ed28ddb9cd85b7102b1b0afc7e04ec709ef5a36b
Mike Coletta <coletta.mike@gmail.com>
2021-04-18 14:35:09 -0700

    Added HTML routes

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const Workout = require("../models/workout.js")
 const Exercise = require("../models/exercise.js")
 
 router.get("api/workouts", (req, res) => {
-    Workout.find({})
+    Workout.find()
     .sort({ date: -1 })
     .then(dbWorkouts => {
         res.json(dbWorkouts);
@@ -13,15 +13,15 @@ router.get("api/workouts", (req, res) => {
     });
 });
 
-router.get("api/exercise/", (req, res) => {
-    Exercise.find({})
-    .then(dbExercise => {
-        res.json(dbExercise);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
-});
+// router.get("api/exercise", (req, res) => {
+//     Exercise.find()
+//     .then(dbExercise => {
+//         res.json(dbExercise);
+//     })
+//     .catch(err => {
+//         res.status(400).json(err);
+//     });
+// });
 
 
 module.exports = router;
